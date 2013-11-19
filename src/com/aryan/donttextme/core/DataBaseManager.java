be4ff10bc66896c05612b20f16e4e1e124a3d49b
shayan-aryan <4evercg@gmail.com>
2013-11-19 12:14:37 -0800

    writing database

--- src/com/aryan/donttextme/core/DataBaseManager.java
+++ src/com/aryan/donttextme/core/DataBaseManager.java
@@ -1,5 +1,6 @@
 package com.aryan.donttextme.core;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,20 +10,48 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DataBaseManager extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "DontText";
+    private static final String DATABASE_NAME = "dont_text";
     private static final int DATABASE_VERSION = 1;
+    private static final String lock = "lock";
+    private static final String TABLE_INBOX = "inbox";
+    private static final String TABLE_BLACKLIST_SPECIFIC_NUMBER = "specific_number_blacklist";
+    private static final String TABLE_BLACKLIST_STARTING_NUMBER = "starting_number_blacklist";
+    private static final String TABLE_BLACKLIST_CONTAINING_STRING = "containing_string_blacklist";
+    private static final String TABLE_BLACKLIST_FROM_TO_NUMBER = "from_to_number_blacklist";
+    private static final String TABLE_SETTINGS = "settings";
+
+
+    private static final String COLUMN_TIME = "time";
+    private static final String COLUMN_ADDRESS = "address";
+    private static final String COLUMN_BODY = "body";
+    private static final String COLUMN_FILTER = "body";
+
+    private static final String CREATE_STATEMENT_SMS = String.format("CREATE TABLE  %s (%s TEXT PRIMARY KEY, %s TEXT, %s TEXT)",
+            TABLE_INBOX, COLUMN_TIME, COLUMN_ADDRESS, COLUMN_BODY);
 
     public DataBaseManager(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_STATEMENT_SMS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
 
+    }
+
+    public void AddToInbox(String time, String address, String body) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        synchronized (lock) {
+            cv.put(COLUMN_TIME, time);
+            cv.put(COLUMN_ADDRESS, address);
+            cv.put(COLUMN_BODY, body);
+            db.insert(TABLE_INBOX, null, cv);
+        }
+        db.close();
     }
 }
